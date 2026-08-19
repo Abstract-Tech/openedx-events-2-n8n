5061215dea2b040711f4e416786b76b2ce856c4b
Emad Rad <codewithemad@gmail.com>
2026-08-19 12:04:47 +0200

    chore: update version to 0.5.0

--- openedx_events_2_n8n/__init__.py
+++ openedx_events_2_n8n/__init__.py
@@ -2,6 +2,6 @@
 This repository contains real-life use cases for Open edX Events..
 """
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 default_app_config = "openedx_events_2_n8n.apps.OpenedxEvents2N8nConfig"
